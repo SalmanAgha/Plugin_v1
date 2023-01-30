ecb2666219bbb3639e8a04c128b0b0098516288e
SalmanAgha <github@tajgasoline.com>
2023-01-30 17:12:49 +0500

    Second commit

--- wp-content/plugins/Salman.php
+++ wp-content/plugins/Salman.php
@@ -12,7 +12,30 @@ register_activation_hook('__FILE__','Salman_Plugin_Activation');
 register_deactivation_hook('__FILE__','Salman_Plugin_Deactivation');
 
 function Salman_Plugin_Activation(){
-	echo "Plugin Activated";
+	echo "Plugin Activatedss";
+	global $wpdb;
+	global $table_prefix;
+
+	$tablename=$table_prefix.'salman_plugin';
+	$Sql="CREATE TABLE `".$tablename."` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `".$tablename."`
+  ADD PRIMARY KEY (`id`);
+  ALTER TABLE `".$tablename."`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
+
+  echo $Sql;
+
+$wpdb->query($Sql);
+
+
+
+
+
 }
 
 function Salman_Plugin_Deactivation(){
